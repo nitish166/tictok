c201f403062c27b07959014f3d8ddeb331840693
unknown <knitish123@gmail.com>
2021-10-03 22:07:14 +0530

    Footer & sideBar

--- tiktok-fronend/src/Video.js
+++ tiktok-fronend/src/Video.js
@@ -1,14 +1,20 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./Video.css";
+import VideoFooter from "./VideoFooter";
+import VideoSidebar from "./VideoSidebar";
 
 function Video() {
+  const [playing, setPlaying] = useState(false);
   const videoRef = useRef(null);
 
   const handleVideoPress = () => {
-    // if video is playing
-    // stop it...
-    //otherwise if its not playing
-    // play it
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.play();
+      setPlaying(true);
+    }
   };
   return (
     <div className="video">
@@ -19,6 +25,8 @@ function Video() {
         ref={videoRef}
         src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
       ></video>
+      <VideoFooter />
+      <VideoSidebar />
     </div>
   );
 }
---
+++ tiktok-fronend/src/VideoSidebar.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./VideoSidebar.css";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MessageIcon from "@material-ui/icons/Message";
+import ShareIcon from "@material-ui/icons/Share";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+
+function VideoSidebar() {
+  const [liked, setLiked] = useState(false);
+  return (
+    <div className="videoSidebar">
+      <div className="videoSidebar__button">
+        {liked ? (
+          <FavoriteIcon fontSize="large" onClick={(e) => setLiked(false)} />
+        ) : (
+          <FavoriteBorderIcon
+            fontSize="large"
+            onClick={(e) => setLiked(true)}
+          />
+        )}
+        <p>{liked ? 101 : 100}</p>
+      </div>
+      <div className="videoSidebar__button">
+        <MessageIcon fontSize="large" />
+        <p>250</p>
+      </div>
+      <div className="videoSidebar__button">
+        <ShareIcon fontSize="large" />
+        <p>12</p>
+      </div>
+    </div>
+  );
+}
+
+export default VideoSidebar;
