8a3180bca28c8449d8b516313ff14045c15455e7
unknown <knitish123@gmail.com>
2021-10-03 12:59:19 +0530

    Readme Updated

--- tiktok-fronend/src/Video.js
+++ tiktok-fronend/src/Video.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Video.css";
 
 function Video() {
+  const videoRef = useRef(null);
+
+  const handleVideoPress = () => {
+    // if video is playing
+    // stop it...
+    //otherwise if its not playing
+    // play it
+  };
   return (
     <div className="video">
       <video
+        onClick={handleVideoPress}
         className="video__player"
         loop
+        ref={videoRef}
         src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
       ></video>
     </div>
